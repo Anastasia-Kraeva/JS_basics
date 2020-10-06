8b8c73152df3361f8e457af71cf36a143bbc5dfa
Kraeva Anastasia <kraeva811625@gmail.com>
2020-10-06 14:07:44 +0300

    lesson 1

---
+++ main.js
@@ -0,0 +1,15 @@
+let Tc = 20;
+let Tf = (9 / 5) * Tc + 32;
+alert(Tf)
+
+let admin = '';
+let name = ("Василиса");
+admin = name;
+alert(admin)
+
+1000108
+console.log(1000 + "108")
+
+// document.write('<img.src="async_vs_defer.jpg">')
+//попыталась вывести изображение
+
